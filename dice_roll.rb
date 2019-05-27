ebcde3d33f3bb41cdc899918991fc38ff72cba48
magical-flow-3186 <rregalado17@gmail.com>
2019-05-27 16:42:04 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,7 @@
+def roll()
+ puts rand(1..7)
+end
+
 # Create method `roll` that returns a random number between 1 and 6
 
 # Feel free to google "how to generate a random number in ruby"
-
-def roll
-  # code goes here
-end
